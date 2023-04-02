e5c01d6c263189d431efa8de4c9a87120d0bf523
raygdev <101662915+raygdev@users.noreply.github.com>
2023-04-01 23:50:58 -0400

    Reformat and add catch all
    add a catch all route and send a 404

--- server/server.js
+++ server/server.js
@@ -18,21 +18,23 @@ const helmet = require("helmet");
 const port = process.env.NODE_PORT || 8080;
 
 //import the data array for vans
-const data  = require("./data.js")
+const data = require("./data.js");
 
 //app.use allows for the server to use the middleware called inside for every route
-app.use(cors({origin:false}));
+app.use(cors({ origin: false }));
 app.disable("x-powered-by");
 
 //in case of url queries. Won't be part of this app though
 app.use(express.urlencoded({ extended: false }));
 //checks for body in request object otherwise request.body is undefined
 
-app.use(express.json({
-    type(req){
-        return true
-    }
-}));
+app.use(
+  express.json({
+    type(req) {
+      return true;
+    },
+  })
+);
 
 /**
  * -POST-
@@ -40,67 +42,67 @@ app.use(express.json({
  */
 
 app.post("/api/login", (req, res, next) => {
-    const { email, password } = req.body
-            // This is an extremely naive version of authentication. Please don't
-            // do this in the real world, and never save raw text passwords
-            // in your database 😇
-    const user = {...data.user}
+  const { email, password } = req.body;
+  // This is an extremely naive version of authentication. Please don't
+  // do this in the real world, and never save raw text passwords
+  // in your database 😇
+  const user = { ...data.user };
 
-            if (!email || !password) {
-                res.message = "Both fields required"
-                return res.status(422).json({ message: "Both fields required" })
-            } else if((user.email === email) && (user.password === password)){
-                // At the very least, don't send the password back to the client 😅
-                delete user.password
-                return res.status(200).json({
-                    user,
-                    token: "Enjoy your pizza"
-                })
-            } else {
+  if (!email || !password) {
+    res.message = "Both fields required";
+    return res.status(422).json({ message: "Both fields required" });
+  } else if (user.email === email && user.password === password) {
+    // At the very least, don't send the password back to the client 😅
+    delete user.password;
+    return res.status(200).json({
+      user,
+      token: "Enjoy your pizza",
+    });
+  } else {
+    res.message = "No user with those credentials found!";
+    return res
+      .status(404)
+      .json({ message: "No user with those credentials found!" });
+  }
+});
 
-                res.message = "No user with those credentials found!"
-                return res.status(404).json({ message: "No user with those credentials found!" })
-            }
+/**
+ * -GET-
+ *  gets all vans or a specific van if id is passed
+ */
+app.get("/api/vans/:id?", (req, res, next) => {
+  if (req.params.id) {
+    let van = data.vans.find((van) => van.id === req.params.id);
+    console.log(van);
+    return res.json({ vans: van });
+  }
+  res.json({ vans: data.vans });
+});
 
-           
-  });
-  
-
-  /**
-   * -GET-
-   *  gets all vans or a specific van if id is passed
-   */
-  app.get("/api/vans/:id?", (req, res, next) => {
-      
-      if(req.params.id){
-        let van = data.vans.find(van => van.id === req.params.id)
-        console.log(van)
-        return res.json({vans:van})
-      }
-      res.json({vans:data.vans})
-      
-  });
-
-  /**
-   * -GET-
-   * gets all vans or a specific van if id is passed
-   */
-  app.get("/api/host/vans/:id?", (req, res, next) => {
-      
-    if(req.params.id){
-        let foundVan = data.vans.find(van => van.hostId === req.params.id)
-        if(!foundVan){
-            console.log("no van found")
-            const message = "We can't seem to find what you are looking for!"
-            res.message = message
-            return res.status(404).json({message})
-        } else {
-            return res.json({vans:foundVan})
-        }
+/**
+ * -GET-
+ * gets all vans or a specific van if id is passed
+ */
+app.get("/api/host/vans/:id?", (req, res, next) => {
+  if (req.params.id) {
+    let foundVan = data.vans.find((van) => van.hostId === req.params.id);
+    if (!foundVan) {
+      const message = "We can't seem to find what you are looking for!";
+      res.message = message;
+      return res.status(404).json({ message });
+    } else {
+      return res.json({ vans: foundVan });
     }
-    return res.json({vans: data.vans})
-  })
+  }
+  return res.json({ vans: data.vans });
+});
+
+app.use("*", (req, res) => {
+  const message = "Could't find what you were looking for!";
+  res.message = message;
+  res.status(404).json({ message });
+});
 
 app.listen(port, () => {
-    console.log(`app is listening on port: ${port} \n this has been fun!`)
-})
+  console.log(`app is listening on port: ${port} \n this has been fun!`);
+});
